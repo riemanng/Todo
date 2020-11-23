821b1a1f7a17ed8c0f3a44436ea81c132a87286e
Konstantin Doronin <doroninkonstantin22@gmail.com>
2020-11-23 14:44:44 +0500

    Initial Commit

---
+++ Todo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todo
+//
+//  Created by Константин Доронин on 23.11.2020.
+//  Copyright © 2020 Константин Доронин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
